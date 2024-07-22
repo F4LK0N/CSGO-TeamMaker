d42d3052a69a02e9a81100a47523d8bb43775b1d
F4LK0N <otaviosoria@gmail.com>
2024-07-22 04:17:43 -0300

    PlayerSkill

--- src/tests/Unit/2/PlayerSkillTest.php
+++ src/tests/Unit/2/PlayerSkillTest.php
@@ -11,12 +11,12 @@ class PlayerSkillTest extends TestCase
     public function testValueInvalid()
     {
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage("Invalid value for 'value': '101'");
-        new PlayerSkill(1, 101);
-        
-        $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Invalid value for 'value': '-1'");
         new PlayerSkill(1, -1);
+        
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Invalid value for 'value': '101'");
+        new PlayerSkill(1, 101);
         
         $this->expectException(InvalidArgumentException::class);
         $this->expectExceptionMessage("Invalid value for 'value': 'a'");
@@ -31,8 +31,40 @@ class PlayerSkillTest extends TestCase
         new PlayerSkill(1, false);
     }
     
-    //public function testSkillNameInvalid()
-    //{
-    //    
-    //}
+    public function testValue()
+    {
+        for ($i=0; $i<=100; $i++) {
+            $skill = new PlayerSkill(1, $i);
+            $this->assertEquals($i, $skill->getValue());
+        }
+    }
+    
+    public function testSkillInvalid()
+    {
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Invalid value for 'skill': 'invalid'");
+        $skill = new PlayerSkill('invalid', 100);
+    }
+    
+    public function testSkill()
+    {
+        $skills = [
+            'defense' => 0,
+            'attack' => 1,
+            'speed' => 2,
+            'strength' => 3,
+            'stamina' => 4
+        ];
+
+        foreach ($skills as $skill => $id) {
+            $playerSkill = new PlayerSkill($skill, 100);
+            $this->assertEquals($skill, $playerSkill->getSkill()->getName());
+        }
+    }
+    
+    public function testJson()
+    {
+        $playerSkill = new PlayerSkill('defense', 100);
+        $this->assertJsonStringEqualsJsonString('{"skill":"defense","value":100}', json_encode($playerSkill));
+    }
 }
