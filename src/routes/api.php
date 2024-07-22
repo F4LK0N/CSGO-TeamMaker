7cce1c7320937297554c55ab44481de78584cb27
F4LK0N <otaviosoria@gmail.com>
2024-07-22 08:22:12 -0300

    API Routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlayerController;
 
-Route::get('/player', function (Request $request) {
-    return $request->user();
+Route::prefix('players')->group(function () {
+    Route::get('/', [PlayerController::class, 'list']);
+    Route::put('/', [PlayerController::class, 'add']);
+    Route::post('/', [PlayerController::class, 'add']);
+    Route::put('/{id}', [PlayerController::class, 'edit']);
+    Route::post('/{id}', [PlayerController::class, 'edit']);
+    Route::delete('/{id}', [PlayerController::class, 'remove']);
 });
