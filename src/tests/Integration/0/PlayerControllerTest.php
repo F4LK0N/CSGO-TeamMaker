6813dd68c2c54f9faba12c453336668aeaf7357b
F4LK0N <otaviosoria@gmail.com>
2024-07-23 04:13:36 -0300

    PlayerController

--- src/tests/Integration/0/PlayerControllerTest.php
+++ src/tests/Integration/0/PlayerControllerTest.php
@@ -81,6 +81,167 @@ class PlayerControllerTest extends TestCase
         ]);
     }
     
+    public function testPlayerSkillsInvalid(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => 'invalid',
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'playerSkills': non-array.",
+        ]);
+    }
+    
+    public function testPlayerSkillsEmpty(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [],
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'playerSkills': empty-array.",
+        ]);
+    }
+    
+    public function testPlayerSkillsSkillMissing(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['value' => 80]
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'skill': Missing field.",
+        ]);
+    }
+    
+    public function testPlayerSkillsSkillInvalid(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'invalid', 'value' => 80]
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'skill': 'invalid'",
+        ]);
+    }
+    
+    public function testPlayerSkillsValueMissing(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'attack']
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'value': Missing field.",
+        ]);
+    }
+    
+    public function testPlayerSkillsValueInvalid(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'attack', 'value' => -1]
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'value': '-1'",
+        ]);
+    }
+    
+    public function testPlayerSkillsValueInvalid2(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'attack', 'value' => 101]
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'value': '101'",
+        ]);
+    }
+    
+    public function testPlayerSkillsDuplicated(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'attack', 'value' => 80],
+                ['skill' => 'attack', 'value' => 90],
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'playerSkills': duplicated skill 'attack'.",
+        ]);
+    }
+    
+    public function testPlayerSkillsDuplicated2(): void
+    {
+        self::truncateTables();
+        
+        $response = $this->postJson('/api/player', [
+            'name' => 'John Doe',
+            'position' => 'midfielder',
+            'playerSkills' => [
+                ['skill' => 'attack', 'value' => 80],
+                ['skill' => 'speed', 'value' => 90],
+                ['skill' => 'attack', 'value' => 100],
+            ]
+        ]);
+
+        $response->assertStatus(400);
+        $response->assertJson([
+            'message' => "Invalid value for 'playerSkills': duplicated skill 'attack'.",
+        ]);
+    }
+    
 //    public function testAdd(): void
 //    {
 //        self::truncateTables();
